7ad77b00d3a000c9dbfd3d255a90aaa8a21ddd02
Joshua Cochran <joshuamcochran@gmail.com>
2017-06-15 12:27:36 -0700

    Initial menu system

--- Multiplayer Pong/main.cpp
+++ Multiplayer Pong/main.cpp
@@ -1,17 +1,44 @@
 #include "SFML/Network.hpp"
+#include "MainMenu.h"
+
+enum GameState {
+	ShowingSplash, Paused, ShowingMenu, Playing, Exiting
+};
+
+void ShowMenu(GameState& gameState, sf::RenderWindow& mainWindow)
+{
+	MainMenu mainMenu;
+	MainMenu::MenuResult result = mainMenu.Show(mainWindow);
+	switch (result)
+	{
+	case MainMenu::Exit:
+		gameState = Exiting;
+		break;
+	case MainMenu::SinglePlayer:
+		break;
+	case MainMenu::Multiplayer:
+		break;
+	case MainMenu::Nothing:
+		break;
+	}
+}
 
 int main(int argc, char* argv[])
 {
+	bool isExiting = false;
 
-	enum GameState {
-		ShowingSplash, Paused, ShowingMenu, Playing, Exiting
-	};
+	GameState _gameState = ShowingMenu;
+	const int SCREEN_WIDTH = 1024;
+	const int SCREEN_HEIGHT = 768;
+	
+	sf::RenderWindow _mainWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32), "Pong");
+	_mainWindow.setFramerateLimit(60);
 
-	bool IsExiting = false;
+	sf::View view;
+	view.reset(sf::FloatRect(24, 24, 256, 192));
+	view.setViewport(sf::FloatRect(0.f, 0.f, 0.75, 1.f));
 
-	GameState _gameState = ShowingSplash;
-
-	while (!IsExiting)
+	while (!isExiting)
 	{
 		switch (_gameState)
 		{
@@ -22,13 +49,14 @@ int main(int argc, char* argv[])
 			break;
 
 		case ShowingMenu:
+			ShowMenu(_gameState, _mainWindow);
 			break;
 
 		case Playing:
 			break;
 		
 		case Exiting:
-			IsExiting = true;
+			isExiting = true;
 			break;
 		}
 	}
@@ -36,3 +64,4 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+
